4fb6f5bbea7b57ae2ddd5f11e7cb9bc6981b5286
vladinSon <vladinson009@icloud.com>
2025-03-07 22:13:53 +0100

    removed unused imports after refactoring

--- 05.architecture-exercises/03.SoftTerest/src/app.js
+++ 05.architecture-exercises/03.SoftTerest/src/app.js
@@ -1,11 +1,9 @@
 const main = document.getElementById('main');
 const nav = document.querySelector('nav');
 import { get } from './api/fetchApi.js';
-import { updateNavBar } from './updateNavBar.js';
-import { clearUserData, getUserData } from './utils/userData.js';
+import { clearUserData } from './utils/userData.js';
 import { createView } from './views/createView.js';
 import { dashboardView } from './views/dashboardView.js';
-import { detailsView } from './views/detailsView.js';
 import { homeView } from './views/homeView.js';
 import { loginView } from './views/loginView.js';
 import { registerView } from './views/registerView.js';
